70862a9e8a544131c1fa5bc7abe13c572e31be05
Martina <martina.gabrielli@yahoo.it>
2024-01-14 18:35:27 +0000

    update nav logo path

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ export default function NavBar () {
     return (
         <>
         <nav className="navbar">
-            <img src="src/assets/airbnb-logo.svg" alt="Airbnb logo" />
+            <img src="/assets/airbnb-logo.svg" alt="Airbnb logo" />
         </nav>
         </>
     );
