8f76e0a4f58c277101c06d291f3ded5095b292d4
Martina <martina.gabrielli@yahoo.it>
2024-01-14 18:33:48 +0000

    update paths

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 export default function Card () {
     return (
         <div className="card">
-            <img src="public/assets/katie-zaferes.png" className="card--image" alt="Main image" />
+            <img src="/assets/katie-zaferes.png" className="card--image" alt="Main image" />
             <div className="card--stats">
-                <img src="public/assets/star.png" alt="Star icon"/>
+                <img src="/assets/star.png" alt="Star icon"/>
                 <span>5.0</span>
                 <span>(6) • </span>
                 <span>USA</span>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export default function NavBar () {
     return (
         <>
         <section className="hero">
-            <img className="hero__image" src="public/assets/hero.png" alt="Hero" />
+            <img className="hero__image" src="/assets/hero.png" alt="Hero" />
             <div className="hero__text-content">
                 <h1 className="hero__heading">Online Experiences</h1>
                 <p className="hero__text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
